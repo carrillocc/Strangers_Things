7be1b363f9c40c4bdb00af7fe29ece7fba729769
Cindy Carrillo <carrillo201994@gmail.com>
2022-10-19 17:43:21 -0500

    trying to export data to Posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { fetchPosts } from "../api/api";
 
-const Posts = ({posts}) => {
+const Posts = (props) => {
+    const { posts } = props
+    console.log('this is posts', posts);
+
     const data = ["hat", "shoe", "shirt", "shorts"]
     const elements = [<div>hat</div>, <div>shoe</div>, <div>shirt</div>]
+
     return (
         <React.Fragment>
             {
