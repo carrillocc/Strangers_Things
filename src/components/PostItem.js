57fce44074815ad0cfdd6cee88193d619f15e35e
Cindy Carrillo <carrillo201994@gmail.com>
2022-10-24 00:14:43 -0500

    added styling

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PostItem = ({posts}) => {
     return (
@@ -8,9 +9,20 @@ const PostItem = ({posts}) => {
                     {posts.author.username}
                 </div>
                 <div className="centered aligned description">
+                    <b>
+                        {posts.title}
+                    </b>
                     <p>
                         {posts.description}
                     </p>
+                    <b>
+                        {posts.price}
+                    </b>
+                    <div className="extra content">
+                        <div className="center aligned header">
+                            <Link to="">View Location</Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
