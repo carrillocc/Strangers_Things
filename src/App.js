744187f45d08406e7ca5ebc7f438e03447f7305b
Cindy Carrillo <carrillo201994@gmail.com>
2022-10-24 01:17:49 -0500

    started log in page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import { Home, Posts } from "./components"
+import { Home, Posts, AccountForm } from "./components"
 import { Route, Switch, Link } from "react-router-dom";
 import { fetchPosts } from "./api/api";
 import "./App.css"
@@ -25,6 +25,7 @@ const App = () => {
             <nav className="ui secondary menu">
                 <Link className="item" to="/">Home</Link>
                 <Link className="item" to="/Posts">Posts</Link>
+                <Link className="item" to="/account">Log In</Link>
             </nav>
             <Switch>
                 <Route exact path="/">
@@ -32,6 +33,9 @@ const App = () => {
                 </Route>
                 <Route className="item" path="/Posts">
                     <Posts posts={posts} />
+                </Route>
+                <Route className="item" path="/account">
+                    <AccountForm />
                 </Route>
             </Switch>
         </div>
---
+++ src/components/AccountForm.js
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+
+const AccountForm = () => {
+        const [username, setUsername] = useState("");
+        const [password, setPassword] = useState("");
+
+    return (
+        <form className="ui form">
+            <h1>Sign Up</h1>
+            <div className="field">
+                <label>Username</label>
+                <input type="text" value={username} placeholder="username" minLength="3" required onChange={(event) => setUsername(event.target.value)} />
+            </div>
+            <div className="field">
+                <label>Password</label>
+                <input type="text" value={password} placeholder="password" minLength="8" required onChange={(event) => setPassword(event.target.value)} />
+            </div>
+            <button className="ui button" type="submit">Sign Up</button>
+        </form>
+    )
+}
+
+export default AccountForm
