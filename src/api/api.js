6d974543191fc966b469067bb03df7c44aa70e9d
Cindy Carrillo <carrillo201994@gmail.com>
2022-10-24 00:48:01 -0500

    regiter user route

--- src/api/api.js
+++ src/api/api.js
@@ -13,4 +13,26 @@ export const fetchPosts = async () => {
     }
 }
 
-export default fetchPosts
+export const registerUser = async (username, password) => {
+    try {
+    const response = await fetch(`${BASEURL}/users/register`, { method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      user: {
+        username,
+        password,
+      },
+    }),
+    });
+    console.log("response...", response)
+    const data = await response.json();
+    console.log("data....", data)
+    return data;
+} catch(error) {
+    console.error("There was an error registering the user,", error);
+}
+};
+
+// export default fetchPosts
