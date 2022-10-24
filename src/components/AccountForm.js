7650b65161e544f7e688bc5418dce9755dd2fa69
Cindy Carrillo <carrillo201994@gmail.com>
2022-10-24 11:27:29 -0500

    able to generate tokens

--- src/components/AccountForm.js
+++ src/components/AccountForm.js
@@ -1,11 +1,23 @@
 import React, {useState} from "react";
+import {registerUser} from "../api/api";
 
 const AccountForm = () => {
         const [username, setUsername] = useState("");
         const [password, setPassword] = useState("");
 
+
+    const onSubmitHandler = async(event) => {
+        event.preventDefault();
+        try {
+            const {data} = await registerUser(username, password)
+         } catch(error) {
+            console.error(error)
+         }
+    };
+
+
     return (
-        <form className="ui form">
+        <form className="ui form" onSubmit={onSubmitHandler}>
             <h1>Sign Up</h1>
             <div className="field">
                 <label>Username</label>
@@ -13,7 +25,7 @@ const AccountForm = () => {
             </div>
             <div className="field">
                 <label>Password</label>
-                <input type="text" value={password} placeholder="password" minLength="8" required onChange={(event) => setPassword(event.target.value)} />
+                <input type="password" value={password} placeholder="password" minLength="8" required onChange={(event) => setPassword(event.target.value)} />
             </div>
             <button className="ui button" type="submit">Sign Up</button>
         </form>
