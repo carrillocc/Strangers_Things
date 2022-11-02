6c7cd0d15d33d56959f4cbe36f4dd59ffab480ed
Cindy Carrillo <carrillo201994@gmail.com>
2022-11-02 15:32:50 -0500

    search bar working

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,12 +1,44 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import PostItem from "./PostItem";
 import {Link} from 'react-router-dom';
 import { deletePost } from "../api/api";
 import './Posts.css';
 
 const Posts = ({ posts, setPosts, token }) => {
-    console.log("use these posts", posts);
+    // console.log("use these posts", posts);
     const [searchTerm, setSearchTerm] = useState('');
+    const [filteredPosts, setFilteredPosts] = useState(posts);
+
+
+    useEffect(() => {
+
+        if (searchTerm) {
+
+            const searchTerms = searchTerm.toLowerCase().trim().split(' ');
+            const filtered = posts.filter((postObject) => {
+                
+                const filterableValues = [
+                    postObject.description,
+                    postObject.location
+                ];
+
+                for (let value of filterableValues) {
+                    const valueLower = value.toLowerCase().trim();
+
+                    for (let term of searchTerms) {
+
+                        if (valueLower.length > 0 && term.length > 0 && valueLower.includes(term)) {
+                            return true;
+                    }
+                }
+            }
+                return false;
+            });
+            setFilteredPosts(filtered)
+        } else {
+            setFilteredPosts(posts)
+        }
+    }, [searchTerm, posts]);
 
     const handleDeleteClick = async (postId) => {
         await deletePost(token, postId);
@@ -15,15 +47,16 @@ const Posts = ({ posts, setPosts, token }) => {
     };
     
     return (<>
-        <div class="ui icon input">
+
+        <div className="ui icon input">
             <input type="text" placeholder="Search..." value={searchTerm} onChange={(event) => {
             setSearchTerm(event.target.value)}} />
-            <i class="search icon"></i>
+            <i className="search icon"></i>
         </div>
 
         <Link to="/Posts/create" className="ui button">Create a Post</Link>
          <div className="posts-container">
-            {posts.map((item) => {
+            {filteredPosts.map((item) => {
                 return (  
                     <PostItem key={item._id} posts={item} headerElement={
                         item.isAuthor ? (
